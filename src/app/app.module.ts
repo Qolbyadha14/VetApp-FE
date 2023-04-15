f9f76c5e38a6f5e28a9725821d03da941a997c42
Qol-By <Qolbyadha04@gmail.com>
2023-04-15 11:22:32 +0700

    feat : update list components and action search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,35 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppointmentListComponent } from './components/appointment-list/appointment-list.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatTableModule } from '@angular/material/table';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppointmentListComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatFormFieldModule,
+        MatButtonModule,
+        MatCardModule,
+        MatDatepickerModule,
+        MatInputModule,
+        MatNativeDateModule,
+        MatTableModule,
+        FormsModule,
+        HttpClientModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/components/appointment-list/appointment-list.component.ts
+++ src/app/components/appointment-list/appointment-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Appointment, AppointmentSearch, AppointmentService } from 'src/app/services/appointment.service';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.css']
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit {
+  appointments: Appointment[] = [];
+  search: AppointmentSearch = {};
 
+  constructor(private appointmentService: AppointmentService) {}
+
+  ngOnInit(): void {
+    this.fetchAppointments();
+  }
+
+  fetchAppointments(): void {
+    this.appointmentService.getAppointments(this.search).subscribe((appointments: any) => {
+      console.log(appointments.data);
+      this.appointments = appointments.data;
+    });
+  }
 }
